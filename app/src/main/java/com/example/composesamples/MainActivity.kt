f5893d6902cbc4bf37643b43d0ab02e3e006b2ad
Raghunandan Kavi <raghunandankavi2010@gmail.com>
2025-04-26 10:15:05 +0530

    Initial commit

---
+++ app/src/main/java/com/example/composesamples/MainActivity.kt
@@ -0,0 +1,97 @@
+package com.example.composesamples
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.material3.Button
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+import com.example.composesamples.ui.theme.ComposeSamplesTheme
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ComposeSamplesTheme {
+
+                val navController = rememberNavController()
+                NavHost(navController = navController, startDestination = Screen.Main.route) {
+                    composable(route = Screen.Main.route) {
+                        MainScreen { route ->
+                            navController.navigate(route = route)
+                        }
+                    }
+                    composable(route = Screen.EffectOrderScreen.route) {
+                        EffectOrder()
+                    }
+                }
+            }
+        }
+    }
+}
+
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun MainScreen(modifier: Modifier = Modifier, navigateTo: (String) -> Unit) {
+    Scaffold(
+        modifier = Modifier.fillMaxSize(),
+        topBar = {
+            TopAppBar(
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.primaryContainer,
+                    titleContentColor = MaterialTheme.colorScheme.primary,
+                ),
+                title = {
+                    Text("Compose Example")
+                })
+        }
+    ){ innerPadding ->
+        LazyColumn(modifier = Modifier
+            .fillMaxSize()
+            .padding(innerPadding)
+            .padding(16.dp)) {
+            items(1) {
+                Button(modifier = Modifier.fillMaxWidth(), onClick = {
+                    navigateTo(Screen.EffectOrderScreen.route)
+                }, content = {
+                    Text(
+                        text = "Effect Order",
+                        modifier = modifier
+                    )
+                }
+                )
+                Spacer(modifier = Modifier.height(16.dp))
+            }
+        }
+    }
+
+}
+
+@Preview(showBackground = true)
+@Composable
+fun GreetingPreview() {
+    ComposeSamplesTheme {
+        MainScreen {
+
+        }
+    }
+}
